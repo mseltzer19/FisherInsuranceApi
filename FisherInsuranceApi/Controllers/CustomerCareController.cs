e2ae031319ab077fa179e1f8e4aa36334a45d124
mseltzer19 <seltzer.19@buckeyemail.osu.edu>
2017-02-05 17:42:56 -0500

    update

---
+++ FisherInsuranceApi/Controllers/CustomerCareController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/claims/quotes")] 
+    public class CustomerCareController : Controller 
+    { 
+        // POST api/claims/quotes 
+        [HttpPost] 
+        public IActionResult Post([FromBody]string value) 
+        { 
+            return Created("", value); 
+        }
+
+         // GET api/claims/quotes/5 
+        [HttpGet("{id}")] 
+        public IActionResult Get(int id) 
+        { 
+            return Ok("The id is: " + id); 
+        }
+
+          // PUT api/claims/quotes/id 
+        [HttpPut("{id}")] 
+        public IActionResult Put(int id, [FromBody]string value) 
+        { 
+            return NoContent(); 
+        } 
+
+         // DELETE api/claims/quotes/id 
+        [HttpDelete("{id}")] 
+        public IActionResult Delete(int id) 
+        { 
+            return Delete(id); 
+        }
+    }
