2f540e8725cea20d2da966c6d07d7b481239c796
mseltzer19 <seltzer.19@buckeyemail.osu.edu>
2017-02-26 05:01:23 -0500

    push

---
+++ FisherInsuranceApi/wwwroot/app/components/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'nav-bar',
+    //template: '<nav-bar>',
+    //templateUrl: './app/components/navbar/navbar.component.html'//,
+    styleUrls: ['./app/components/navbar/navbar.component.css']
+    //templateUrl: './app/components/navbar/navbar.component.css'
+})
+export class NavBarComponent {
+} 
