a5cdb9ba684624ee109e4881f9c669604f9704fb
mseltzer19 <seltzer.19@buckeyemail.osu.edu>
2017-03-31 17:08:08 -0400

    update

--- FisherInsuranceApi/Startup.cs
+++ FisherInsuranceApi/Startup.cs
@@ -1,56 +1,163 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using FisherInsuranceApi.Data;
+
+using FisherInsuranceApi.Security;
+
 using Microsoft.AspNetCore.Builder;
+
 using Microsoft.AspNetCore.Hosting;
+
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
 using Microsoft.Extensions.Configuration;
+
 using Microsoft.Extensions.DependencyInjection;
+
 using Microsoft.Extensions.Logging;
 
+using Microsoft.IdentityModel.Tokens;
+
+
+
 namespace FisherInsuranceApi
+
 {
+
     public class Startup
+
     {
+
         public Startup(IHostingEnvironment env)
+
         {
+
             var builder = new ConfigurationBuilder()
+
                 .SetBasePath(env.ContentRootPath)
+
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+
                 .AddEnvironmentVariables();
+
             Configuration = builder.Build();
+
         }
+
+
 
         public IConfigurationRoot Configuration { get; }
 
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
+
         public void ConfigureServices(IServiceCollection services)
+
         {
-            // Add framework services.
-            //services.AddSingleton<IMemoryStore, MemoryStore>(); 
-            
+
+
+
             services.AddMvc();
-            services.AddIdentity<Applicationuser, IdentityRole>(config=>
+
+
+
+            services.AddIdentity<Applicationuser, IdentityRole>(config =>
+
             {
+
                 config.User.RequireUniqueEmail = true;
+
                 config.Password.RequireNonAlphanumeric = false;
+
+                config.Cookies.ApplicationCookie.AutomaticChallenge = false;
+
             })
+
             .AddEntityFrameworkStores<FisherContext>()
+
             .AddDefaultTokenProviders();
+
+
+
             services.AddDbContext<FisherContext>();
+
+            
+
         }
 
+
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+
         {
-            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            //loggerFactory.AddDebug();
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+
+            loggerFactory.AddDebug();
+
+
+
             app.UseDefaultFiles();
-            app.UseStaticFiles(); 
+
+            app.UseStaticFiles();
+
+
+
+            //add our JwtProvider that we created
+
+            app.UseJwtProvider();
+
+
+
+            //add the built in authentication 
+
+            app.UseJwtBearerAuthentication(new JwtBearerOptions()
+
+            {
+
+                AutomaticAuthenticate = true,
+
+                AutomaticChallenge = true,
+
+                RequireHttpsMetadata = false,
+
+                TokenValidationParameters = new TokenValidationParameters()
+
+                {
+
+                    IssuerSigningKey = JwtProvider.SecurityKey,
+
+                    ValidIssuer = JwtProvider.Issuer,
+
+                    ValidateIssuerSigningKey = true,
+
+                    ValidateIssuer = false,
+
+                    ValidateAudience = false
+
+                }
+
+            });
+
+
+
+            //if we wanted to support cookies, we do this:
+
+            //app.UseIdentity();
+
+            //or this
+
+            //app.UseCookieAuthentication();
+
+
+
             app.UseMvc();
+
         }
+
     }
+
 }
--- FisherInsuranceApi/wwwroot/app/components/navbar/navbar.component.ts
+++ FisherInsuranceApi/wwwroot/app/components/navbar/navbar.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
+import { Routes, RouterModule } from "@angular/router";
+import { AuthService } from "../../auth.service";
+//import { Router } from "../../app.routing";
 @Component({
     selector: 'nav-bar',
     templateUrl: './app/components/navbar/navbar.component.html',
     styleUrls: ['./app/components/navbar/navbar.component.css']
 })
-export class NavBarComponent {} 
+export class NavBarComponent {
+    constructor(public router: Routes, public authService: AuthService) { }
+    isActive(data: any[]): boolean {
+        return this.router.isActive(
+            this.router.createUrlTree(data),
+            true);
+    }
+    logout(): boolean {
+        // logs out the user, then redirects him to Welcome View.
+        if (this.authService.logout()) {
+            this.router.navigate([""]);
+        }
+        return false;
+    }
+
+} 
